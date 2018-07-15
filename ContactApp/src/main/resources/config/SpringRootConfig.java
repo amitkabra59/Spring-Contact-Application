efc946f912fb7228b38e7222e08036e83a0c3828
amitkabra59 <amitkabra59@gmail.com>
2018-07-15 20:12:40 +0200

    Spring MVC not working yet

--- ContactApp/src/main/resources/config/SpringRootConfig.java
+++ ContactApp/src/main/resources/config/SpringRootConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"src/java/resources"})
+@ComponentScan(basePackages = {"resources"})
 public class SpringRootConfig {
 
 	//TODO - Services, DAO, Data SOurce, Email Sender, other business beans
---
+++ ContactApp/src/main/resources/config/SpringWebConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan(basePackages={"resources"})
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//TODO
+	}
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setViewClass(JstlView.class);
+		vr.setPrefix("/WEB-INF/view/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+}
